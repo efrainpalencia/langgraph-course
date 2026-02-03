023f58aba0d9cdb6acdec854e81b855b4b396288
Efrain Palencia <efrainpalencia@hotmail.com>
2026-02-03 08:34:52 -0500

    Creates chains

---
+++ src/chains.py
@@ -0,0 +1,30 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweets."
+            "Always provide detailed recommendations , including requests for length, virality, style, etc."
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a techie twitter influencer assistant tasked with writing excellent twitter posts."
+            "Generate the best twitte posts possible fot the user's request."
+            "If the user provides critique, respond with a revised version of your previous attempts.",
+        ),
+        MessagesPlaceholder(variable_name="messages")
+    ]
+)
+
+llm = ChatOpenAI()
+
+generation_chain = generation_prompt | llm
+reflection_chain = reflection_prompt | llm
