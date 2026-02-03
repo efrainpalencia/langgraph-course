5673d8d8ffba09883d284d4f4728cbaa008b5770
Efrain Palencia <efrainpalencia@hotmail.com>
2026-02-02 23:35:32 -0500

    Creates graph in main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,45 @@
-import os
 from dotenv import load_dotenv
+
+from langgraph.graph import MessagesState, StateGraph, START, END
+from langchain_core.messages import HumanMessage
+
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
 
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
+
+
+def should_continue(state: MessagesState) -> str:
+    """
+    Decide if we should continue the loop or stop based upon whether the LLM made a tool call
+    """
+
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END: END,
+    ACT: ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
 if __name__ == '__main__':
     print("ReACt Function Calling")
+    res = app.invoke({"messages": [HumanMessage(
+        content="What is the temperature in Tokyo? List it and then triple it.")]})
+    print(res["messages"][LAST].content)
