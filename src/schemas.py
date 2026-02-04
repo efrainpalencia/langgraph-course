522cdfd04781000ffde42839a86172788b604318
Efrain Palencia <efrainpalencia@hotmail.com>
2026-02-04 10:12:40 -0500

    Part 2: Reflection agent

--- src/schemas.py
+++ src/schemas.py
@@ -11,14 +11,16 @@ class Reflection(BaseModel):
 class AnswerQuestion(BaseModel):
     """Answer the question."""
 
-    answer: str = Field(description="~250 word detailed answer to the question.")
-    reflection: Reflection = Field(description="Your reflection on the initial answer.")
+    answer: str = Field(
+        description="~250 word detailed answer to the question.")
+    reflection: Reflection = Field(
+        description="Your reflection on the initial answer.")
     search_queries: List[str] = Field(
         description="1-3 search queries for researching improvements to address the critique of your current answer."
     )
 
 
-class ReviseAnswer(BaseModel):
+class ReviseAnswer(AnswerQuestion):
     """Revise your original answer to your question."""
 
     references: List[str] = Field(
--- src/tool_executor.py
+++ src/tool_executor.py
@@ -10,14 +10,15 @@ load_dotenv()
 tavily_tool = TavilySearch(max_results=5)
 
 
-def run_queries(search_queries: list[str]):
+def run_queries(search_queries: list[str], **kwargs):
     """Run the generated queries."""
     return tavily_tool.batch([{"query": query} for query in search_queries])
 
 
 execute_tools = ToolNode(
     [
-        StructuredTool.from_function(run_queries, name=AnswerQuestion.__name__),
-        StructuredTool.from_function(run_queries, ReviseAnswer.__name__),
+        StructuredTool.from_function(
+            run_queries, name=AnswerQuestion.__name__),
+        StructuredTool.from_function(run_queries, name=ReviseAnswer.__name__),
     ]
 )
