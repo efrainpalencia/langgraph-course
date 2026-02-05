389d3c61170b7bb09e5d4dd60e7597ea26462462
Efrain Palencia <efrainpalencia@hotmail.com>
2026-02-04 21:59:08 -0500

    Creates ingestion pipeline to chroma

---
+++ ingestion.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2021-03-21-lm-toxicity/",
+    "https://lilianweng.github.io/posts/2023-01-10-inference-optimization/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+]
+
+docs = [WebBaseLoader(urls).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+
+doc_splits = text_splitter.split_documents(docs_list)
+
+file_path = "./.chroma"
+if os.path.exists(file_path):
+    print(f"A Chroma vector database {file_path} exists.")
+else:
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=OpenAIEmbeddings(),
+        persist_directory="./.chroma"
+    )
+
+retriever = Chroma(
+    collection_name="rag-chroma",
+    embedding_function=OpenAIEmbeddings(),
+    persist_directory="./.chroma"
+).as_retriever()
